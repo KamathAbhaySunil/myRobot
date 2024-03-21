9216bdf46463f9697e9409f8e09eaac57d942928
KamathAbhaySunil <abhaysk23@gmail.com>
2024-03-21 19:06:59 +0530

    added counter

--- src/my_robot_controller/my_robot_controller/my_first_node.py
+++ src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -6,11 +6,12 @@ class MyNode(Node):
 
     def __init__(self):
          super().__init__("first_node")
+         self.counter_ = 0
          self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
-        self.get_logger().info("hello mother fucker")
-
+        self.get_logger().info("hello mother fucker" + str(self.counter_))
+        self.counter_ += 1
 
 
 def main(args=None):
