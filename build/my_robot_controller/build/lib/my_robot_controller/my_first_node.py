5bd8a2a89ce2e3f50e5fc59a5adffe5adf0abc41
KamathAbhaySunil <abhaysk23@gmail.com>
2024-03-21 18:29:24 +0530

    hello mother fucker

--- build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
+++ build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
@@ -6,7 +6,10 @@ class MyNode(Node):
 
     def __init__(self):
          super().__init__("first_node")
-         self.get_logger().info("Hello from ROS2")
+         self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
 
 
 
